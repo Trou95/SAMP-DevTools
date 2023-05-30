c42578c68cef1f8dc4821192aca616175fe8ab97
Trou95 <gorkem_095@hotmail.com>
2023-05-30 12:49:14 +0300

    Inital Commit

---
+++ SAMP-DevTools/main.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS
+
+#include <windows.h>
+#include <string>
+#include <assert.h>
+#include <process.h>
+
+#include "SAMPFUNCS_API.h"
+#include "game_api\game_api.h"
